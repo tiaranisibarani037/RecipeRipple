954959980139e4670d4ab03a439e84965b874cc2
Jufourlisasirait <112596570+Jufourlisasirait@users.noreply.github.com>
2024-10-28 16:09:03 +0700

    test

--- resources/views/notifikasiPage.blade.php
+++ resources/views/notifikasiPage.blade.php
@@ -212,7 +212,7 @@
                   </a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="#" style="display: flex; flex-direction: column; align-items: center;">
+                  <a class="nav-link" href="searchresep" style="display: flex; flex-direction: column; align-items: center;">
                       <i class="fas fa-search"></i>
                       Cari
                   </a>
